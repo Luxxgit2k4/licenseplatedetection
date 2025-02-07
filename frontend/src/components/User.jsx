5c86a0925d691c86e5acbc3a5304a4ae7fe595b3
bupd <bupdprasanth@gmail.com>
2025-02-08 01:18:34 +0530

    add user jsx componenet
    Signed-off-by: bupd <bupdprasanth@gmail.com>

---
+++ frontend/src/components/User.jsx
@@ -0,0 +1,203 @@
+import { useState } from "react";
+
+const MyComponent = ({ registerUrl }) => {
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target); // Get the FormData from the form
+
+    // Extract email and password from FormData
+    const email = String(formData.get("email")); // Convert to string explicitly
+    const password = String(formData.get("password")); // Convert to string explicitly
+
+    // Example logging
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+
+    const data = {
+      email: email,
+      password: password,
+      booked_parking_slots: {},
+    };
+
+    // Send data to backend
+    try {
+      const response = await fetch(registerUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data), // Send form data to backend
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        console.log(result);
+        setSuccessMessage("User registered successfully!");
+        alert(successMessage)
+        window.location = "/"
+      } else {
+        setErrorMessage("Failed to register user.");
+      }
+    } catch (error) {
+      setErrorMessage("An error occurred while registering.");
+    }
+  };
+
+  return (
+    <div>
+      <div class="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+          <p class="mt-2 text-center text-sm text-gray-600 max-w">
+            Or
+            <a href="#" class="font-medium text-blue-600 hover:text-blue-500">
+              create an account
+            </a>
+          </p>
+        </div>
+
+        <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <form class="space-y-6" onSubmit={handleFormSubmit}>
+              <div>
+                <label
+                  for="email"
+                  class="block text-sm font-medium text-gray-700"
+                >
+                  Email address
+                </label>
+                <div class="mt-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autocomplete="email"
+                    required
+                    class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Enter your email address"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  for="password"
+                  class="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <div class="mt-1">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autocomplete="current-password"
+                    required
+                    class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Enter your password"
+                  />
+                </div>
+              </div>
+
+              <div class="flex items-center justify-between">
+                <div class="flex items-center">
+                  <input
+                    id="remember_me"
+                    name="remember_me"
+                    type="checkbox"
+                    class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                  />
+                  <label
+                    for="remember_me"
+                    class="ml-2 block text-sm text-gray-900"
+                  >
+                    Remember me
+                  </label>
+                </div>
+
+                <div class="text-sm">
+                  <a
+                    href="#"
+                    class="font-medium text-blue-600 hover:text-blue-500"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+            <div class="mt-6">
+              <div class="relative">
+                <div class="absolute inset-0 flex items-center">
+                  <div class="w-full border-t border-gray-300"></div>
+                </div>
+                <div class="relative flex justify-center text-sm">
+                  <span class="px-2 bg-gray-100 text-gray-500">
+                    Or continue with
+                  </span>
+                </div>
+              </div>
+
+              <div class="mt-6 grid grid-cols-3 gap-3">
+                <div>
+                  <a
+                    href="#"
+                    class="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                  >
+                    <img
+                      class="h-5 w-5"
+                      src="https://www.svgrepo.com/show/512120/facebook-176.svg"
+                      alt=""
+                    />
+                  </a>
+                </div>
+                <div>
+                  <a
+                    href="#"
+                    class="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                  >
+                    <img
+                      class="h-5 w-5"
+                      src="https://www.svgrepo.com/show/513008/twitter-154.svg"
+                      alt=""
+                    />
+                  </a>
+                </div>
+                <div>
+                  <a
+                    href="#"
+                    class="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                  >
+                    <img class="h-6 w-6" src="/google.svg" alt="" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Conditional rendering of error and success messages */}
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+      {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
+    </div>
+  );
+};
+
+export default MyComponent;
