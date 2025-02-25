21584d7e2c0b8d623d0501a2ee71f9965116679a
Luxxgit2k4 <lakshmanoni1234@gmail.com>
2025-02-25 17:10:32 +0530

    Changed flex-clo into flex-col lol
    Signed-off-by: Luxxgit2k4 <lakshmanoni1234@gmail.com>

--- frontend/src/components/Parking.jsx
+++ frontend/src/components/Parking.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const Parking = () => {
   return (
-    <div className="flex flex-clo items-center bg-gray-900 text-white min-h-screen p-4">
+    <div className="flex flex-col items-center bg-gray-900 text-white min-h-screen p-4">
     <h1 className="text-xl font-bold mb-4">Parking Slot</h1>
     <div className="flex gap-4 mb-8">
     <button className="px-3 py-2 bg-yellow-500 text-black rounded-md text-sm font-medium"> G Floor
