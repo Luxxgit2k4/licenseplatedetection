b72f96f01651ae34fd84ed26f42058428528cc12
Luxxgit2k4 <lakshmanoni1234@gmail.com>
2025-02-25 17:02:58 +0530

    Added Basic ui
    Signed-off-by: Luxxgit2k4 <lakshmanoni1234@gmail.com>

---
+++ frontend/src/components/Parking.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const Parking = () => {
+  return (
+    <div className="flex flex-clo items-center bg-gray-900 text-white min-h-screen p-4">
+    <h1 className="text-xl font-bold mb-4">Parking Slot</h1>
+    <div className="flex gap-4 mb-8">
+    <button className="px-3 py-2 bg-yellow-500 text-black rounded-md text-sm font-medium"> G Floor
+    </button>
+
+    <button className="px-3 py-2 bg-gray-800 rounded-md text-sm hover:bg-gray-700">
+    1st Floor
+    </button>
+
+    <button className="px-3 py-2 bg-gray-800 rounded-md text-sm hover:bg-gray-700">
+    2nd Floor
+    </button>
+
+    <button className="px-3 py-2 bg-gray-800 rounded-md text-sm hover:bg-gray-700">
+    3rd Floor
+    </button>
+    </div>
+    </div>
+  );
+};
+
+export default Parking;
