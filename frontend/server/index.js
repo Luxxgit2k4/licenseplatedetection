1ba802053ed01368fa83e70a68cc8c24cd7ebde6
Luxxgit2k4 <lakshmanoni1234@gmail.com>
2025-03-06 23:48:39 +0530

    backend done for razorpay
    Signed-off-by: Luxxgit2k4 <lakshmanoni1234@gmail.com>

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import Razorpay from "razorpay";
 import cors from "cors";
 import { configDotenv } from "dotenv";
+import crypto from "crypto";
 import bodyParser from "body-parser";
 
 configDotenv() // To load razorpay api key from the .env
@@ -19,7 +20,7 @@ app.post("/create-order", async (req, res) => {
   const { amount } = req.body;
   try {
     const order = await razorpay.orders.create({
-      amount: amount * 100,
+      amount: amount,
       currency: "INR",
       payment_capture: 1,
     });
@@ -29,3 +30,18 @@ app.post("/create-order", async (req, res) => {
     res.status(500).json({ error: "Failed to create order"});
   }
 });
+
+app.post("/verify-payment", async (req, res) => {
+  const { razorpay_payment_id, razorpay_order_id, razorpay_signature } = req.body;
+  const sha = crypto.createHmac("sha256", process.env.RAZORPAY_SECRET);
+  sha.update(`${razorpay_order_id}|${razorpay_payment_id}`);
+  const digest = sha.digest("hex");
+  if (digest !== razorpay_signature) {
+    return res.status(400).json({ msg: "Transaction is not legit!"});
+  }
+
+ res.json({ message: "Success", orderId: razorpay_order_id, paymentId: razorpay_payment_id, });
+});
+
+const PORT = 8007;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
