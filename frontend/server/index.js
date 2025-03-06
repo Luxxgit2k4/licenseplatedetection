0d6ce9b1db725601807d33d8880555460a7ace87
Luxxgit2k4 <lakshmanoni1234@gmail.com>
2025-03-06 22:36:03 +0530

    Loaded the api keys from .env
    Signed-off-by: Luxxgit2k4 <lakshmanoni1234@gmail.com>

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -1,7 +1,16 @@
 import express from "express";
 import Razorpay from "razorpay";
 import cors from "cors";
-import dotenv from "dotenv";
+import { configDotenv } from "dotenv";
 import bodyParser from "body-parser";
 
-dotenv.config();
+configDotenv() // To load razorpay api key from the .env
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_SECRET,
+})
