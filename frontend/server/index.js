f95f50d85d59fe9fc35bb858428e3bc196e2cbaa
Luxxgit2k4 <lakshmanoni1234@gmail.com>
2025-03-06 22:42:17 +0530

    added a post request to create an order in the razorpay
    Signed-off-by: Luxxgit2k4 <lakshmanoni1234@gmail.com>

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -13,4 +13,19 @@ app.use(bodyParser.json());
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_SECRET,
-})
+});
+
+app.post("/create-order", async (req, res) => {
+  const { amount } = req.body;
+  try {
+    const order = await razorpay.orders.create({
+      amount: amount * 100,
+      currency: "INR",
+      payment_capture: 1,
+    });
+    res.json({ orderId: order.id });
+  } catch (error) {
+    console.error("Error creating order:", error);
+    res.status(500).json({ error: "Failed to create order"});
+  }
+});
